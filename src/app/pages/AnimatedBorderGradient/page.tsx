37e8b97d31347cc329489ec223941e004b03d0a6
Chinedu Okeke <chiscookeke11@gmail.com>
2025-08-07 21:05:13 +0100

    added gradient border

---
+++ src/app/pages/AnimatedBorderGradient/page.tsx
@@ -0,0 +1,19 @@
+
+
+
+
+
+export default function Page() {
+    return (
+        <div className="w-full h-screen flex items-center justify-center" >
+
+
+
+            <div className="w-[200px] h-[200px] bg-gray-600 p-1 rounded-xl relative wrapper overflow-hidden " >
+                <div className=" flex items-center justify-center  bg-gray-700 h-[calc(100%-2px)]   w-[calc(100%-2px)]  rounded-xl top-[1px] left-[1px]  absolute " >
+                    The center
+                </div>
+            </div>
+        </div>
+    )
+}
